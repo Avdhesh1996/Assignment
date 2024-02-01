4d946870cff8476cc514c07a882ef006e5413486
Avdhesh1996 <avdheshpandeyit@gmail.com>
2024-02-01 17:49:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/NavigationBar.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+  return (
+    <div className="navbar-container">
+     <div className='navbar-1'>
+     <img className='search-icon' src="/images/search.png" alt="Search-Icon" />
+     <button className='btn-1'>CONNECT</button>
+      </div> 
+     
+    <div className='navbar-2'>
+     
+        <button className='btn-2'>DCA</button>
+      
+       
+       <ul className='content-items'>
+             
+              <li>Spot Grid</li>
+              <li>Futures Grid</li>
+              <li>Martingale</li>
+              <li>Infinity Grid</li>
+              
+             </ul>
+      
+       
+       <img className='icon-1' src="/images/stock-market.png" alt="stock-market" />
+       <img className='icon-2' src="/images/forex.png" alt="forex-icon" />
+      
+      </div> 
+
+      <div className="navbar-3"></div>
+      
+      <div className="navbar-4">
+        <div  className='lastdiv-1'>
+        <img className='icon-3' src="/images/trading.png" alt="trading-icon" />
+        <p>ETH/INRTD<br /><span className='span-1'>Ethereum</span></p> 
+      
+        </div>
+     
+     
+      <button className='btn-4'>$20,679.17<br /><span className='span-2'>$20,679.17</span></button>
+      <div  className='lastdiv-2'>
+      <img className='play-image'  src="/images/play.png" alt="play-button" />
+      <p className='tutorial'>Tutorial</p> 
+      </div>
+      <div  className='lastdiv-3'>
+      <img className='book-image' src="/images/book.png" alt="book" />
+      <p className='Guidance'>Guidance</p> 
+
+     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  </div>
+
+  );
+};
+
+
+export default NavigationBar;
---
+++ src/VerticalMenu.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import './VerticalMenu.css';
+
+const VerticalMenu = () => {
+
+  return (
+    <div className="menu-container">
+    
+        <div className='company-info'>
+          <img className='meta' src="/images/meta.png" alt="meta" />   
+         <div className="company-name">CRIPTIC</div>
+         </div>
+      <div className="user-info">
+             <img className='admin'  src="/images/admin.webp" alt="admin" /> 
+              <div className="user-name">Avdhesh<br />Kumar Pandey</div>
+              <div className="user-role">admin</div>
+            </div>
+            <div className="menu-items">
+           
+              <div className='home'>
+              <img className='home-icon'src="/images/home.png" alt="home" /> 
+              <p>Home</p>
+              </div>
+              <div className='buy'>
+              <img className='buy-icon'src="/images/buy.png" alt="buy" /> 
+              <p>Live Pricing</p>
+
+              </div>
+              <div  className='bot'>
+              <img className='bot-icon'src="/images/trading-bot.png" alt="bot" /> 
+              <p> Trading Bot </p>
+              </div>
+             
+              <div className='swap'>
+
+              <img className='swap-icon'src="/images/swap.png" alt="swap" /> 
+              <p>Swap</p>
+
+
+              </div>
+
+              <div className='liquidity'>
+              <img className='liquidity-icon'src="/images/liquidity.png"  /> 
+              <p>Liquidity</p>
+
+
+                </div>
+              
+              <div className='profile' >
+              <img className='profile-icon'src="/images/profile.png"  /> 
+              <p>Profile</p>
+
+              </div>
+
+
+              <div  className='vote'>
+              <img className='vote-icon'src="/images/vote.png"  /> 
+              <p>Vote</p>
+
+              </div>
+              <div  className='lock'>
+
+              <img className='lock-icon'src="/images/lock.png"  /> 
+              <p>Authentication</p>
+
+              </div>
+             
+            
+          </div>
+       
+      </div>
+     
+    
+  );
+};
+
+export default VerticalMenu;
+
